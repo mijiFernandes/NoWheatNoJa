60880ec3ee42894e834d0ece1777205ab843c174
Jay Oh <jay@Jayui-MacBookPro.local>
2023-02-18 02:56:04 +0900

    Minor change

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -7,6 +7,7 @@ import { StackItem } from '@channel.io/bezier-react';
 import { Button } from '@channel.io/bezier-react';
 import { ButtonColorVariant } from '@channel.io/bezier-react';
 import { ButtonStyleVariant } from '@channel.io/bezier-react';
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
 
@@ -19,71 +20,73 @@ export default function SignUp() {
       opacity: "0.5"
     }}><img src="http://localhost:3000/5.jpeg" /></div>
     <div style={{width: "100%", height: "100vh", position: "absolute", top: 0, left: 0, zIndex: 1 }}></div>
-  <VStack
-    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', minHeight: '100vh',
-    position: "absolute", top: 0, left: 0, zIndex: 10 }}
-    align="center"
-  >
-    <StackItem
-      size={50}
-      style={{ width: '300px'}}
-    >
-      <TextField
-      ref={{
-        current: '[Circular]'
-      }}
-      allowClear
-      autoFocus
-      maxLength={25}
-      onChange={function noRefCheck(){}}
-      placeholder="Please write down your Email"
-      size={36}
-      variant={0}
-    />
-    </StackItem>
-    <StackItem
-      size={50}
-      style={{ width: '300px'}}
-    >
-      <TextField
-      ref={{
-        current: '[Circular]'
-      }}
-      allowClear
-      autoFocus
-      maxLength={20}
-      onChange={function noRefCheck(){}}
-      placeholder="Please write down your PW"
-      size={36}
-      variant={0}
-      />
-    </StackItem>
-    <StackItem
-      size={50}
-      style={{ width: '300px'}}
-    >
-      <TextField
-      ref={{
-        current: '[Circular]'
-      }}
-      allowClear
-      autoFocus
-      maxLength={25}
-      onChange={function noRefCheck(){}}
-      placeholder="Please write down your User Name"
-      size={36}
-      variant={0}
-    />
-    </StackItem>
-    <StackItem>
-    <Button
-      text="Sign Up"
-      colorVariant={ButtonColorVariant.Green}
-      styleVariant={ButtonStyleVariant.Primary}
-    />
-  </StackItem>
-  </VStack>
-  </div>
+      <VStack
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', minHeight: '100vh',
+        position: "absolute", top: 0, left: 0, zIndex: 10 }}
+        align="center"
+      >
+        <StackItem
+          size={50}
+          style={{ width: '300px'}}
+        >
+          <TextField
+          ref={{
+            current: '[Circular]'
+          }}
+          allowClear
+          autoFocus
+          maxLength={25}
+          onChange={function noRefCheck(){}}
+          placeholder="Please write down your Email"
+          size={36}
+          variant={0}
+        />
+        </StackItem>
+        <StackItem
+          size={50}
+          style={{ width: '300px'}}
+        >
+          <TextField
+          ref={{
+            current: '[Circular]'
+          }}
+          allowClear
+          autoFocus
+          maxLength={20}
+          onChange={function noRefCheck(){}}
+          placeholder="Please write down your PW"
+          size={36}
+          variant={0}
+          />
+        </StackItem>
+        <StackItem
+          size={50}
+          style={{ width: '300px'}}
+        >
+          <TextField
+          ref={{
+            current: '[Circular]'
+          }}
+          allowClear
+          autoFocus
+          maxLength={25}
+          onChange={function noRefCheck(){}}
+          placeholder="Please write down your User Name"
+          size={36}
+          variant={0}
+        />
+        </StackItem>
+          <StackItem>
+            <Link to="/login">
+              <Button
+                text="Sign Up"
+                colorVariant={ButtonColorVariant.Green}
+                styleVariant={ButtonStyleVariant.Primary}
+              />
+            </Link>
+        </StackItem>
+      </VStack>
+    </div>
 
 
 
