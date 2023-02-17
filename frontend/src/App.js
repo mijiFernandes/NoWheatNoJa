7f3bef2608660fac301aee7a40ab9a2821258371
Jay Oh <jay@Jayui-MacBookPro.local>
2023-02-18 02:13:23 +0900

    human win

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
-import MyPage from "./pages/MyPage";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Router>
-        <div>
+        <div style={{ height: '100%', width: '100%' }}>
           <Routes>
             <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="/my" element={<MyPage />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
             <Route path="/" element={<MainPage />}></Route>
           </Routes>
         </div>
